6bbab501ee33a0bde1f14ed836a5d87bfcef58eb
QuyDoan <casso.station10@gmail.com>
2026-02-23 16:06:52 +0700

    refactor: update student entity

--- src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/entity/Student.java
+++ src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/entity/Student.java
@@ -1,12 +1,10 @@
 package vn.edu.hcmut.cse.adsoftweng.lab.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "students")
@@ -18,8 +16,14 @@ public class Student {
     @Id
     private String id;
 
+    @Column(unique = true, nullable = false)
+    private String studentId;
+
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private int age;
 
 }
